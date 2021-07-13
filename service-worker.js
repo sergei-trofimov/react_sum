47d062e801a5762b08423e84b171610e26b21dab
sergei-trofimov <feast.st@gmail.com>
2021-07-13 18:33:50 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.2c8e5fc34215d4c651a7ae2cc6e18c6e.js"
 );
 
 self.addEventListener('message', (event) => {
